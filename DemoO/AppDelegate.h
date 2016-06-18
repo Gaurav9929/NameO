6cd00da1117ccdcc4f45be7d2a29c2c1f02f77d2
Omega Solutions <b>
2016-06-18 12:42:59 +0530

    hi

---
+++ DemoO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoO
+//
+//  Created by Omega Solutions on 04/06/16.
+//  Copyright © 2016 Omega. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoO/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  DemoO
+//
+//  Created by Omega Solutions on 04/06/16.
+//  Copyright © 2016 Omega. All rights reserved.
+// datas
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    
+    NSLog(@"hi");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
